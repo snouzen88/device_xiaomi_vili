6d0890e82890f688f66ec3598b08594504defd14
snouzen88 <snouzen88@gmail.com>
2025-04-06 18:14:22 +0800

    vili: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@ echo 'Cloning stuff needed to build for vili'
 
 # Device common
 echo 'Cloning common device tree'
-git clone https://github.com/AOSP-for-vili/device_xiaomi_sm8350-common.git -b lineage-22.2-wip device/xiaomi/sm8350-common
+git clone https://github.com/snouzen88/device_xiaomi_sm8350-common.git -b lineage-22.2 device/xiaomi/sm8350-common
 
 # Kernel
 echo 'Cloning kernel'
@@ -11,15 +11,15 @@ git clone https://github.com/AOSP-for-vili/android_kernel_xiaomi_sm8350.git --re
 # Hardware Xiaomi
 echo 'Cloning hardware xiaomi'
 rm -rf hardware/xiaomi
-git clone https://github.com/AOSP-for-vili/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
+git clone https://github.com/blazerpaul15/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
 
 # Vendor
 echo 'Cloning vendor tree'
-git clone https://github.com/AOSP-for-vili/vendor_xiaomi_vili.git -b lineage-22.2-wip vendor/xiaomi/vili
+git clone https://github.com/snouzen88/vendor_xiaomi_vili.git -b lineage-22.2 vendor/xiaomi/vili
 
 # Vendor common
 echo 'Cloning common vendor tree'
-git clone https://github.com/AOSP-for-vili/vendor_xiaomi_sm8350-common.git -b lineage-22.2-wip vendor/xiaomi/sm8350-common
+git clone https://github.com/snouzen88vendor_xiaomi_sm8350-common.git -b lineage-22.2 vendor/xiaomi/sm8350-common
 
 # Meme camera
 echo 'Cloning meme camera'
